2c9fd3bacc517f2a5c41e4fa66eac449174cac0d
Mufeed Kcp <bitengineguy@gmail.com>
2026-05-07 22:31:44 +0530

    add cron job log file to the repo

--- ingest_watchdog.sh
+++ ingest_watchdog.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-find ./incoming -type f -name "*.csv" | while read -r file; do
-    echo "Processing $file"
+find /mnt/c/Users/biten/OneDrive/Documents/linux_project_sales_files/incoming -type f -name "*.csv" | while read -r file; do
+    /usr/bin/echo "Processing $file"
 
 filename=$(basename "$file")
-before_filename="$file"
 
 if [ -f $file ]
 then
     gzip  $file
-    echo "Sucessfully Compressed: $filename" >> ./logs/data_ingestion.log
-    mv "$file.gz" "./archive/${filename}.gz_$(date +%Y%m%d_H%M%S)"
-    echo "$filename moved to archive"
-    echo "$(date): Archived $filename" >> ./logs/data_ingestion.log
-    echo "Renamed $before_filename into $filename" >> ./logs/data_ingestion.log
+    /usr/bin/echo "Sucessfully Compressed: $filename" >> /mnt/c/Users/biten/OneDrive/Documents/linux_project_sales_files/logs/data_ingestion.log
+    mv "$file.gz" "/mnt/c/Users/biten/OneDrive/Documents/linux_project_sales_files/archive/${filename}.gz_$(/usr/bin/date +%Y%m%d_H%M%S)"
+    /usr/bin/echo "$filename moved to archive"
+    /usr/bin/echo "$(/usr/bin/date): Archived $filename" >> /mnt/c/Users/biten/OneDrive/Documents/linux_project_sales_files/logs/data_ingestion.log
 fi
 done
 
